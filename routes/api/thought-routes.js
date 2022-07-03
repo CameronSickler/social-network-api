4e44c5ce8aa2ff187715ee8a815d631e0c5c32f6
Cameron <sicklecj@gmail.com>
2022-07-03 17:29:07 -0500

    add user routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const {
+
     createThought,
     getAllThoughts,
     getThoughtById,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,34 @@
 const router = require('express').Router();
 
-//code here
+const {
+
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+
+} = require('../../controllers/user-controller');
+
+
+//Set up GET all and POST
+router.route('/').get(getAllUsers)
+    .post(createUser);
+
+
+//Set up GET, PUT, and DELETE
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+
+//Set up POST and DELETE
+router.route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
 
 module.exports = router; 
