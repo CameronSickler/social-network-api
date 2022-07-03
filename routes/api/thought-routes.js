7276913b2215e922864fef4333b3d84e31d0be12
Cameron <sicklecj@gmail.com>
2022-07-03 17:22:21 -0500

    add thought controller

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,42 @@
 const router = require('express').Router();
 
-//code here
+const {
+    createThought,
+    getAllThoughts,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+
+} = require('../../controllers/thought-controller');
+
+
+//Set up GET all
+router.route('/')
+    .get(getAllThoughts);
+
+
+//Set up GET one, PUT, and DELETE by ID
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+
+//Set up POST thought
+router.route('/:userId')
+    .post(createThought);
+
+
+//Set up POST reaction
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+
+//Set up DELTE reaction
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
 
 module.exports = router;
